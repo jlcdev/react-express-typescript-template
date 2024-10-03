0b32fc49562a467bffbdb40a1a4a5651dd3fd379
jlcdev <potnox@gmail.com>
2024-10-03 22:07:20 +0200

    CHANGE repository name ADD frontend with react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Helloworld from "./components/Helloworld"
+
+function App() {
+
+  return (
+    <main>
+      <Helloworld />
+    </main>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Helloworld.tsx
@@ -0,0 +1,7 @@
+function Helloworld() {
+    return (
+      <h1>Hello World from React App</h1>
+    )
+  }
+  
+  export default Helloworld
